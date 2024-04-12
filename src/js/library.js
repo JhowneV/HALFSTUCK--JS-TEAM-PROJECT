bed35f4a783714adc0adb8899dccee88da7a9e78
Joan Viray <virayjoan1985@gmail.com>
2024-04-12 08:11:50 +0800

    update watched libray.js

--- src/js/library.js
+++ src/js/library.js
@@ -20,11 +20,11 @@ const options = {
 };
 
 
-const form = document.getElementById('lib-buttons');
+// const form = document.getElementById('lib-buttons');
 
 //Watched
 
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
     const watchedBtn = document.getElementById('watched-btn');
     const moviesContainer = document.querySelector('.gallery');
     let watchedMovies = JSON.parse(localStorage.getItem('movie-watched')) || [];
@@ -35,26 +35,25 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     async function getWatchedMovies() {
-        moviesContainer.innerHTML = ''; 
-        for (const movieId of watchedMovies) {
-            try {
-                if (!isValidMovieId(movieId)) {
-                    throw new Error(`Invalid movie ID: ${movieId}`);
-                }
-                const movie = await getMovieDetailsById(movieId);
-                const movieElement = createMovieElement(movie);
-                moviesContainer.appendChild(movieElement);
-    } 
-            catch (error) {
-                if (error.message === 'Movie not found.') {
-                    console.warn(`Movie with ID ${movieId} not found.`);
-                } else {
-                    console.error('Failed to display movie:', error);
-                }
+    moviesContainer.innerHTML = ''; 
+    for (const movieId of watchedMovies) {
+        try {
+            // Check if the movieId is valid
+            if (!isValidMovieId(movieId)) {
+                throw new Error(`Invalid movie ID: ${movieId}`);
+            }
+            const movie = await getMovieDetailsById(movieId);
+            const movieElement = createMovieElement(movie);
+            moviesContainer.appendChild(movieElement);
+        } catch (error) {
+            if (error.message === 'Movie not found.') {
+                console.warn(`Movie with ID ${movieId} not found.`);
+            } else {
+                console.error('Failed to display movie:', error);
             }
         }
     }
-
+}
     async function getMovieDetailsById(movieId) {
     const API_KEY = "b5e824a3d922f68ba211fcf6dbdcb6f5";
     const BASE_URL = 'https://api.themoviedb.org/3';
@@ -92,7 +91,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function isValidMovieId(movieId) {
         return !isNaN(movieId);
     }
-});
+// });
 
 // PAGINATION
 
